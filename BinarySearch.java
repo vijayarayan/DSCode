a2f7f89c342631b0c0316f7e90851836c40c076c
vmisra <vmisra@vmware.com>
2019-02-16 14:03:11 -0800

    binearysearch code

---
+++ BinarySearch.java
@@ -0,0 +1,27 @@
+
+//Binary Search routine 
+/** Initial Condition: left = 0, right = length-1
+  *Termination: left > right
+  *Searching Left: right = mid-1
+  *Searching Right: left = mid+1
+**/
+
+  
+int binarySearch(int[] nums, int target){
+  if(nums == null || nums.length == 0)
+    return -1;
+
+  int left = 0;
+  int right = nums.length - 1;
+
+  while(left <= right){
+    // Prevent (left + right) overflow
+    int mid = left + (right - left) / 2;
+    if(nums[mid] == target){ return mid; }
+    else if(nums[mid] < target) { left = mid + 1; }
+    else { right = mid - 1; }
+  }
+
+  // End Condition: left > right
+  return -1;
+}
